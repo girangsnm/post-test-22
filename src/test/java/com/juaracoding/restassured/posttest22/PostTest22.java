a41eb25b06c13158a92298fc1ca650073074a171
girangsnm <girangsnm123@gmail.com>
2022-08-25 19:18:29 +0700

    PostTest22

---
+++ src/test/java/com/juaracoding/restassured/posttest22/PostTest22.java
@@ -0,0 +1,63 @@
+package com.juaracoding.restassured.posttest22;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
+public class PostTest22 {
+	
+	@Test
+	public void testPut() {
+		JSONObject request = new JSONObject();
+		request.put("Name", "John English");
+		request.put("Job", "SQA Engineer");
+		System.out.println(request.toJSONString());
+		
+		given()
+		.header("Content-Type", "application/json")
+		.contentType(ContentType.JSON)
+		.accept(ContentType.JSON)
+		.body(request.toJSONString())
+		.when()
+		.post("https://mern-backend-8881.herokuapp.com/products")
+		.then()
+		.statusCode(400)
+		.log().all();
+		
+	}
+	
+	@Test
+	public void testPatch() {
+		JSONObject request = new JSONObject();
+		request.put("Name", "John English");
+		request.put("Job", "SQA Engineer");
+		
+		System.out.println(request.toJSONString());
+		
+		given()
+		.header("Content-Type", "application/json")
+		.contentType(ContentType.JSON)
+		.accept(ContentType.JSON)
+		.body(request.toJSONString())
+		.when()
+		.post("https://mern-backend-8881.herokuapp.com/products")
+		.then()
+		.statusCode(400)
+		.log().all();
+		
+	}
+	
+	@Test
+	public void testDelete() {
+		when()
+		.delete("https://mern-backend-8881.herokuapp.com/products")
+		.then()
+		.statusCode(404)
+		.log().all();
+	}
+
+}
